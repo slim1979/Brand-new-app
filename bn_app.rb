fec017042f587f5105c5e85960cebc0ef308f377
chinezan <chinezan@yandex.ru>
2017-02-07 22:39:40 +0400

    добавлены комментарии, добавлен User_name в базу комментов

--- bn_app.rb
+++ bn_app.rb
@@ -27,6 +27,7 @@ configure do
 				(
 					id          INTEGER PRIMARY KEY AUTOINCREMENT,
 					CreatedDate DATE,
+					user_name   TEXT,
 					Context     TEXT,
 					post_id 	INTEGER
 				)'
@@ -47,9 +48,14 @@ before '/secure/*' do
 end
 
 get '/' do
-
+	
   init_db
+  #добавляем в переменную все посты с аргументом desc для вывода их
+  #на экран в обратном порядке, т.е. самые свежие посты будут наверху списка
   @outpost = @db.execute 'select * from posts order by id desc'
+  
+  @out_comments = @db.execute 'select id from comments order by post_id'
+  
   erb :index
 end
 
@@ -86,43 +92,65 @@ get '/new' do
   erb :new
 end
 
-#обработчик запроса post. браузер отправляет страницу на сервера
+#здесь добавляются и проверяются на заполнение новые посты.
+#обработчик запроса post. браузер отправляет страницу на сервер.
 post '/new' do
 
   @new_post = params[:new_post]
   
+	#если пост пустой, выдается ошибка 
 	if @new_post ==""
 		@error ='Это поле не может быть пустым'
 		return erb :new
 	end
 	
+	#если нет, пост добавляется в базу данных.
 	@db.execute 'insert into 
 				posts (Context, CreatedDate) 
 				values (?, datetime())', [@new_post]
 	
-	@number = @db.execute 'select id from posts order by posts.id desc limit 1'
+	#@number = @db.execute 'select id from posts order by posts.id desc limit 1'
 	
+	#после добавления поста в базу данных происходит перенаправление на главную страницу
 	redirect to '/'
   
 end
 
+#здесь создается страничка для каждого поста с его номером в url
 get '/details/:post_id' do
-
+	
 	post_id = params[:post_id]
 	outpost = @db.execute 'select * from posts where id =?',[post_id]
+	
+	#эта переменная получает из базы все данные на кокретный пост, так как post_id у нас уникален
+	#в представлении details.erb из этой переменной будем получать номер поста, сам текст и дату создания
 	@row = outpost[0]
+	
+	#добавляем в переменную @comments все комментарии к конкретному посту по post_id
+	@comments = @db.execute 'select * from comments where post_id = ? order by id',[post_id]
 	
 	erb :details
 	
 end
-
-post '/details/:post_id' do
-
-	post_id = params[:post_id]
-	@new_post = params[:new_post]
+#здесь добавляются комментарии к публикациям
+post '/details/:post_id' do	
+		
+		#форма запрашивает имя того, кто пишет комментарий
+		#и непосредственно сам комментарий
+		post_id = params[:post_id]
+		@user_name = params[:user_name]
+		@new_post = params[:new_post]		
+		
+		#если имя отправителя или поле комментария пусты, то форма обнуляется
+		#пустой комментарий не добавляется.
+		#к сожалению, пока не получилось вывести ошибку из-за непонимания, как правильно написать вид в erb :
+		if @new_post =="" || @user_name==""	
+			redirect to ('/details/' + post_id) 	
+		end
+	#========конец кода обработки ошибки заполнения полей формы записи.==========
 	@db.execute 'insert into 
-				comments (Context, CreatedDate, post_id) 
-				values (?, datetime(),?)', [@new_post, post_id]
+				comments (user_name, Context, CreatedDate, post_id) 
+				values (?, ?, datetime(),?)', [@user_name, @new_post, post_id]
 	
 	redirect to ('/details/' + post_id) 
 end
